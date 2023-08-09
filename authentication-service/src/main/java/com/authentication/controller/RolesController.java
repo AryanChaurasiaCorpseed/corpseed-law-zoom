e3628de1ea2604578f1b2b1bac61fff94e66c2c4
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-08-09 16:53:24 +0530

    add cors in authentication micro service

--- authentication-service/src/main/java/com/authentication/controller/RolesController.java
+++ authentication-service/src/main/java/com/authentication/controller/RolesController.java
@@ -8,12 +8,13 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @Api("Handle role related actions")
 @RequestMapping("/api/auth/role")
--- authentication-service/src/main/java/com/authentication/controller/SignupController.java
+++ authentication-service/src/main/java/com/authentication/controller/SignupController.java
@@ -8,11 +8,12 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @Api("Handle Signup related actions")
 @RequestMapping("/api/auth/signup")
